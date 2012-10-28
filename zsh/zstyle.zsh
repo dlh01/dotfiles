62cf83e0aae96803151e3d4d1859bb76ba49362c
David Herrera <herrera.dl@gmail.com>
2012-10-27 23:39:11 -0700

    Case-insensitive completion for zsh

--- zsh/zstyle.zsh
+++ zsh/zstyle.zsh
@@ -1,3 +1,6 @@
 
 # Ignore hosts file when autocompleting
 zstyle ':completion:*' hosts off
+
+# case insensitive completion for cd etc
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'   
