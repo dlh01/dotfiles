a4459854decf142df0c54a35ddd420860c132d09
David Herrera <herrera.dl@gmail.com>
2013-03-01 11:01:36 -0800

    update nightly-backup script mysqldump

--- bin/nightly.sh
+++ bin/nightly.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mysqldump --all-databases > ~/temp/nightlies/all-databases.sql
+/Applications/MAMP/Library/bin/mysqldump --host=localhost --all-databases -uroot -proot > ~/temp/nightlies/all-databases.sql
 cat /etc/hosts > ~/temp/nightlies/hosts
 cat /Applications/MAMP/conf/apache/sites > ~/temp/nightlies/sites
